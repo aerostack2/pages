4f17534ce1478b2cd6e783c228858d724f73f5ee
Javilinos <javilinos@pop-os.localdomain>
2022-12-14 14:09:44 +0100

    debugging

--- main.sh
+++ main.sh
@@ -52,24 +52,21 @@ shopt -s dotglob
 shopt -s nullglob
 array=(*/)
 
-for dir in "${array[@]}"; do
-    if [[ -f "$dir/setup.py" ]]; then # This is a python project    
-        echo "$dir is a python project, performing compilation";
-        cp -r $dir $doc_dir/_user/temp_ws/src
-        cd $doc_dir/_user/temp_ws/
-        colcon build --symlink-install
-        source install/setup.bash
-        cd -
-        sphinx-apidoc -o $doc_dir/_user/temp_ws/src/"$dir"docs/source $doc_dir/_user/temp_ws/src/"$dir""$dir"
+cp -r python_interface/ $doc_dir/_user/temp_ws/src
+cd $doc_dir/_user/temp_ws/
+colcon build --symlink-install
+source install/setup.bash
+cd -
 
-    elif [[ -f "$dir/doxygen.dox" ]]; then # This is a c++ project, no need to compile
-        echo "$dir is a c++ project, performing doxygen build";
-        cp -r $dir $doc_dir/_user
-        cd $doc_dir/_user/$dir
-        doxygen doxygen.dox
-        cd -
-    fi;
-    done
+sphinx-apidoc -o $doc_dir/_user/temp_ws/src/python_interface/docs/source $doc_dir/_user/temp_ws/src/python_interface/python_interface
+
+cp -r as2_core/ $doc_dir/_user
+
+cd $doc_dir/_user/as2_core
+
+doxygen doxygen.dox
+
+cd -
 
 echo ::group:: Creating temp directory
 tmp_dir=$(mktemp -d -t pages-XXXXXXXXXX)
