d54c7b8ad4bcb201951dd2874173b4da72f6bb71
Javilinos <javilinos@pop-os.localdomain>
2023-01-09 19:17:58 +0100

    debugging

--- main.sh
+++ main.sh
@@ -77,7 +77,7 @@ for dir in "${arrModules[@]}"; do
 
     shopt -u globstar
 
-    for _folder in $(dirname $(grep -R --exclude=*.py -l "$dir" "$doc_dir")); do
+    for _folder in $(dirname $(grep -R --exclude=*.py "$dir" "$doc_dir")); do
         echo $_folder
         folder=$_folder
     done
